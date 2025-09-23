661f8ea3ba7251a624f6a790794ddf5ee4863aa5
JDPlumbing <jdplumbingsoflo@gmail.com>
2025-09-23 16:43:01 -0400

    added serde support

--- src/types.rs
+++ src/types.rs
@@ -1,11 +1,13 @@
-#[derive(Debug, Clone, Copy)]
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq)]
 pub struct Cartesian {
     pub x: f64,
     pub y: f64,
     pub z: f64,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum CoordSystem {
     Math,     // Z-up (physics)
     Graphics, // Y-up (games/graphics)
